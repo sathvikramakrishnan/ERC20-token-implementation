e20417eeaeed62ef93ce37e1f4b0c5ddb7edc856
Sathvik R <sathvikr2004@gmail.com>
2024-07-15 21:34:57 +0530

    EOD commit

--- src/TokenFactory.sol
+++ src/TokenFactory.sol
@@ -4,4 +4,15 @@ pragma solidity ^0.8.18;
 
 import {MyToken} from "./MyToken.sol";
 
-contract TokenFactory is MyToken {}
+contract MyTokenFactory {
+    event TokenCreated(address indexed creator, address tokenAddress);
+
+    MyToken[] public tokens;
+
+    function createTokens(string memory _name, string memory _symbol) public {
+        MyToken token = new MyToken(_name, _symbol);
+        tokens.push(token);
+
+        emit TokenCreated(msg.sender, address(token));
+    }
+}
