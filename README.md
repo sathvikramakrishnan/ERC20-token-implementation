66d257ba142c3d7a8d3ee67a8faf490559e1da93
Sathvik R <sathvikr2004@gmail.com>
2024-07-16 21:13:17 +0530

    temporary commit 2

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ PS1 final project
 ## Objective
 Write a basic ERC-20 token smart contract in Solidity.\
 To simplify token deployment, use a factory contract `TokenFactory.sol` that deploys new instances of the `MyToken` contract.
+
+## Issues
+1. msg.sender issue in transferFrom
+2. msg.sender issue in transfer
+3. approve() - check condition that _owner is msg.sender
--- src/TokenFactory.sol
+++ src/TokenFactory.sol
@@ -4,15 +4,92 @@ pragma solidity ^0.8.18;
 
 import {MyToken} from "./MyToken.sol";
 
+// 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
+// 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2
+
 contract MyTokenFactory {
     event TokenCreated(address indexed creator, address tokenAddress);
 
-    MyToken[] public tokens;
+    MyToken[] private s_tokens;
+    mapping(string => MyToken) private s_symbolToToken;
 
     function createTokens(string memory _name, string memory _symbol) public {
         MyToken token = new MyToken(_name, _symbol);
-        tokens.push(token);
+        s_tokens.push(token);
+        s_symbolToToken[_symbol] = token;
 
         emit TokenCreated(msg.sender, address(token));
+    }
+
+    // functions that use token address and call functions
+
+    // setters
+    function mint(MyToken _token, uint256 _value) external {
+        _token._mint(msg.sender, _value);
+    }
+
+    function burn(MyToken _token, uint256 _value) external {
+        _token._burn(msg.sender, _value);
+    }
+
+    function approve(
+        MyToken _token,
+        address _owner,
+        address _spender,
+        uint256 _value
+    ) external {
+        _token.approve(_owner, _spender, _value);
+    }
+
+    function transfer(MyToken _token, address _to, uint256 _value) external {
+        _token.transfer(_to, _value);
+    }
+
+    function transferFrom(
+        MyToken _token,
+        address _from,
+        address _to,
+        uint256 _value
+    ) external {
+        _token.transferFrom(_from, _to, _value);
+    }
+
+    // view functions
+    function getToken(uint256 _index) external view returns (MyToken) {
+        return s_tokens[_index];
+    }
+
+    function getTokenFromSymbol(
+        string memory _symbol
+    ) external view returns (MyToken) {
+        return s_symbolToToken[_symbol];
+    }
+
+    function getName(MyToken _token) external view returns (string memory) {
+        return _token.name();
+    }
+
+    function getSymbol(MyToken _token) external view returns (string memory) {
+        return _token.symbol();
+    }
+
+    function getDecimals(MyToken _token) external pure returns (uint256) {
+        return _token.decimals();
+    }
+
+    function getSupply(MyToken _token) external view returns (uint256) {
+        return _token.totalSupply();
+    }
+
+    function getBalance(MyToken _token) external view returns (uint256) {
+        return _token.balanceOf(msg.sender);
+    }
+
+    function getAllowance(
+        MyToken _token,
+        address _owner,
+        address _spender
+    ) external view returns (uint256) {
+        return _token.allowance(_owner, _spender);
     }
 }
